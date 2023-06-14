d4fe6fc92b2f858a61b732379cc03b160c1f556e
ggimenez0115 <ggimenez@academic.rrc.ca>
2023-06-13 20:15:25 -0500

    Initial commit message

---
+++ gimenez_gloriana_ui_garden/src/components/Card/Card.stories.tsx
@@ -0,0 +1,20 @@
+// card.stories.tsx
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+
+import Card from './Card';
+
+export default {
+  title: 'Card',
+  component: Card,
+} as Meta;
+
+const Template: Story<CardProps> = args => <Card {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  title: 'Example Card',
+  content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  backgroundColor: '#fff',
+  disabled: false, // Set the default state of the card here (enabled or disabled)
+};
